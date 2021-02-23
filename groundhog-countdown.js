47bb1d639c78926cb6d7cf29657bc1715fc6b7b3
lfvperes <luisfvperes@gmail.com>
2021-02-23 18:29:41 -0300

    countdown method in nodejs

---
+++ groundhog-countdown.js
@@ -0,0 +1,14 @@
+const today = new Date();
+const this_year = today.getFullYear();
+
+// start as this year's groundhog day
+var groundhog_day = new Date(this_year, 1, 2, 23, 59, 59);
+// if its past, update the year by +1, otherwise does not change
+groundhog_day.setFullYear(this_year + (groundhog_day.getTime() < today.getTime()));
+
+// conversion factor from miliseconds to days
+const conversion = 1000 * 3600 * 24;
+// time remaining to the next groundhog day, in miliseconds
+var remaining = groundhog_day.getTime() - today.getTime();
+// show remaining time in days
+console.log(`${Math.floor(remaining / conversion)} days remaining for Groundhog Day!`);
